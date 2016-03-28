4ca7413ffa612c3e86be0623af3ba83e10e4af5e
Arik Marmorstein <Arik Marmorstein>
2016-03-28 12:08:47 +0300

    initial commit - welcome screen

---
+++ ValaApp/app/src/main/java/com/vala/valaapp/activities/WelcomeActivity.java
@@ -0,0 +1,36 @@
+package com.vala.valaapp.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.vala.valaapp.LoginActivity;
+import com.vala.valaapp.R;
+
+public class WelcomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        Button loginButton = (Button)findViewById(R.id.login_button);
+        Button signupButton = (Button)findViewById(R.id.signup_button);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(WelcomeActivity.this, LoginActivity.class));
+            }
+        });
+
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(WelcomeActivity.this, LoginActivity.class));
+            }
+        });
+    }
+}
