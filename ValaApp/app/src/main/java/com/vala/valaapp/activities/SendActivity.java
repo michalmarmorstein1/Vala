f46fb2d87baddad26c2c9824344c9b06bd008bfe
Arik Marmorstein <Arik Marmorstein>
2016-04-04 21:39:55 +0300

    select receiver dialog skeleton

--- ValaApp/app/src/main/java/com/vala/valaapp/activities/SendActivity.java
+++ ValaApp/app/src/main/java/com/vala/valaapp/activities/SendActivity.java
@@ -1,13 +1,27 @@
 package com.vala.valaapp.activities;
 
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 
 import com.vala.valaapp.R;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class SendActivity extends NavigationDrawerActivity {
 
@@ -20,6 +34,16 @@ public class SendActivity extends NavigationDrawerActivity {
                 R.array.currency_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         currencySpinner.setAdapter(adapter);
+
+        Button selectReceiver = (Button) findViewById(R.id.receiver_editText);
+        selectReceiver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                InviteDialogFragment frag = new InviteDialogFragment();
+                frag.show(ft, "invite_fragment_tag");
+            }
+        });
     }
 
     @Override
@@ -49,4 +73,53 @@ public class SendActivity extends NavigationDrawerActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    static public class InviteDialogFragment extends DialogFragment {
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setStyle(DialogFragment.STYLE_NORMAL, R.style.FullScreenDialog);
+        }
+
+        @Override
+        public void onStart() {
+            super.onStart();
+            Dialog d = getDialog();
+            if (d != null){
+                int width = ViewGroup.LayoutParams.MATCH_PARENT;
+                int height = ViewGroup.LayoutParams.MATCH_PARENT;
+                d.getWindow().setLayout(width, height);
+            }
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, final ViewGroup container, Bundle savedInstanceState) {
+            final View root = inflater.inflate(R.layout.fragment_select_receiver, container, true);
+            Button inviteBtn = (Button) root.findViewById(R.id.invite_button);
+            inviteBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    RadioGroup radiogroup = (RadioGroup) root.findViewById(R.id.radio_group);
+                    // layout params to use when adding each radio button
+                    LinearLayout.LayoutParams layoutParams = new RadioGroup.LayoutParams(
+                            RadioGroup.LayoutParams.WRAP_CONTENT,
+                            RadioGroup.LayoutParams.WRAP_CONTENT);
+
+                    String[] receiversArray = getResources().getStringArray(R.array.receivers_array);
+                    for (int i = 0; i < receiversArray.length; i++){
+                        RadioButton newRadioButton = new RadioButton(getActivity());
+                        String label = receiversArray[i];
+                        newRadioButton.setText(label);
+                        newRadioButton.setId(i);
+                        radiogroup.addView(newRadioButton, layoutParams);
+                    }
+
+                }
+            });
+            return root;
+        }
+
+    }
+
 }
