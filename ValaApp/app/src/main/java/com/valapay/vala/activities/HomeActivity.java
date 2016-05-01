b2657f2ff88d0d2fe56e0e919ef6bf915a3c9ea5
Arik Marmorstein <Arik Marmorstein>
2016-05-01 10:34:17 +0300

    API place holders - Home screen

--- ValaApp/app/src/main/java/com/valapay/vala/activities/HomeActivity.java
+++ ValaApp/app/src/main/java/com/valapay/vala/activities/HomeActivity.java
@@ -1,6 +1,7 @@
 package com.valapay.vala.activities;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -8,6 +9,8 @@ import android.widget.Button;
 import com.valapay.vala.R;
 
 public class HomeActivity extends NavigationDrawerActivity {
+
+    private GetAffiliatesTask getAffiliatesTask = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +20,11 @@ public class HomeActivity extends NavigationDrawerActivity {
         collectButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(HomeActivity.this, PickupLocationActivity.class));
+                if(getAffiliatesTask == null){
+                    //TODO show progress
+                    getAffiliatesTask = new GetAffiliatesTask();
+                    getAffiliatesTask.execute((Void) null);
+                }
             }
         });
 
@@ -33,5 +40,36 @@ public class HomeActivity extends NavigationDrawerActivity {
     @Override
     protected int getContentId() {
         return R.layout.activity_home;
+    }
+
+    public class GetAffiliatesTask extends AsyncTask<Void, Void, Boolean> {
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            // TODO: get affiliates details
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+            getAffiliatesTask = null;
+            //TODO hide progress
+            if (success) {
+                startActivity(new Intent(HomeActivity.this, PickupLocationActivity.class));
+            } else {
+                //TODO show server errors
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+            getAffiliatesTask = null;
+            //TODO hide progress
+        }
     }
 }
