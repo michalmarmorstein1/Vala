267c76620b4c12a59a47f4ac145272cc397aea3f
Arik Marmorstein <Arik Marmorstein>
2016-05-01 12:34:30 +0300

    API place holders - Pick up confirmation screen

--- ValaApp/app/src/main/java/com/valapay/vala/activities/PickupConfirmationActivity.java
+++ ValaApp/app/src/main/java/com/valapay/vala/activities/PickupConfirmationActivity.java
@@ -1,6 +1,7 @@
 package com.valapay.vala.activities;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +12,9 @@ import android.widget.Toast;
 import com.valapay.vala.R;
 
 public class PickupConfirmationActivity extends AppCompatActivity {
+
+    private ConfirmCollectionTask confirmCollectionTask = null;
+    private ReportIssueTask reportIssueTask = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +28,11 @@ public class PickupConfirmationActivity extends AppCompatActivity {
         okBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(PickupConfirmationActivity.this, HomeActivity.class));
+                if(confirmCollectionTask == null){
+                    //TODO show progress
+                    confirmCollectionTask = new ConfirmCollectionTask();
+                    confirmCollectionTask.execute((Void) null);
+                }
             }
         });
 
@@ -32,8 +40,74 @@ public class PickupConfirmationActivity extends AppCompatActivity {
         problemBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(PickupConfirmationActivity.this, "That\'s a shame :)", Toast.LENGTH_SHORT).show();
+                if(reportIssueTask == null){
+                    //TODO show progress
+                    reportIssueTask = new ReportIssueTask();
+                    reportIssueTask.execute((Void) null);
+                }
             }
         });
+    }
+
+    private class ConfirmCollectionTask extends AsyncTask<Void, Void, Boolean> {
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            // TODO: confirm money received
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+            confirmCollectionTask = null;
+            //TODO hide progress
+            if (success) {
+                startActivity(new Intent(PickupConfirmationActivity.this, HomeActivity.class));
+            } else {
+                //TODO show server errors
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+            confirmCollectionTask = null;
+            //TODO hide progress
+        }
+    }
+
+    private class ReportIssueTask extends AsyncTask<Void, Void, Boolean> {
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            // TODO: report issue
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+            reportIssueTask = null;
+            //TODO hide progress
+            if (success) {
+                Toast.makeText(PickupConfirmationActivity.this, "That\'s a shame :)", Toast.LENGTH_SHORT).show();
+            } else {
+                //TODO show server errors
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+            reportIssueTask = null;
+            //TODO hide progress
+        }
     }
 }
